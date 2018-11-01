d82ea79ad09dfa0f27cb65eba03b09afb93937db
ecmerchant <ecmerchant.contact@gmail.com>
2018-11-01 15:49:12 +0900

    upd

--- db/migrate/20181101180429_add_asin_unique_to_products.rb
+++ db/migrate/20181101180429_add_asin_unique_to_products.rb
@@ -2,11 +2,6 @@ class AddAsinUniqueToProducts < ActiveRecord::Migration[5.0]
   def change
     execute <<-SQL
       ALTER TABLE products
-        DROP CONSTRAINT for_asin_upsert
-    SQL
-
-    execute <<-SQL
-      ALTER TABLE products
         ADD CONSTRAINT for_asin_upsert UNIQUE ("asin", "user", "listing_condition");
     SQL
   end
