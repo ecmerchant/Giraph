00d94d5df44c5ceae84e403d6a2db2acfa29e78f
ecmerchant <ecmerchant.contact@gmail.com>
2018-11-08 10:33:07 +0900

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -233,7 +233,7 @@ class Product < ApplicationRecord
         Retryable.retryable(tries: 5, sleep: 2.0) do
           time_counter2 = Time.now.strftime('%s%L').to_i
           diff_time = time_counter2 - time_counter1
-          while diff_time < 850.0 do
+          while diff_time < 900.0 do
             sleep(0.02)
             time_counter2 = Time.now.strftime('%s%L').to_i
             diff_time = time_counter2 - time_counter1
